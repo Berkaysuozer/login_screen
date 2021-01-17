7bf86a555909dd6c24495acb79127d71df636ca4
Berkaysuozer <33202163+Berkaysuozer@users.noreply.github.com>
2021-01-17 07:07:50 +0300

    A simple login screen made by HTML , CSS , JS

---
+++ script.js
@@ -0,0 +1,21 @@
+var a = "berkay"
+var b = "suozer"
+
+
+function login(){
+var username = document.getElementById("username").value 
+var password = document.getElementById("password").value
+
+if(username == a && password ==b  ){
+    console.log("AFERINN")
+    window.location = "berkay.html"
+}
+
+    else{
+console.log("sie mk")
+window.alert("TEKRAR DENE.!")
+
+    }
+}
+
+
